6fffa86a99cf3a3639fffd58a3a0284af46d6df8
Nghia <nghiabeo69@gmail.com>
2023-05-29 20:37:10 +0700

    Update home.php

--- home.php
+++ home.php
@@ -1,15 +1,30 @@
 <?php
+// include("solarSystem.html");
 session_start();
-if ($_SESSION[$_COOKIE["users"]] == "admin"){
-    setcookie("users", "admin", time() + 3600, "/Website");
-    echo "<h1 style='color: green;'> Welcome admin </h1>";
-}elseif ($_SESSION[$_COOKIE["users"]] == "user"){
-    setcookie("users", "user", time() + 3600, "/Website");
-    echo "<h1 style='color: white;'> Welcome user </h1>";
-}else{
-    setcookie("users", "user1", time() + 3600, "/Website");
-    echo "<h1 style='color: white;'>Welcome anonymous</h1>";
+
+function end_Section(){
+
+
+        session_destroy();
+        header("Location: login.php");
 }
+try {
+
+    if (!isset($_SESSION[$_COOKIE["users"]])) {
+        end_Section();
+    } else {
+        if ($_SESSION[$_COOKIE["users"]] == $_SESSION[$_COOKIE["users"]]) {
+            echo "<h1 style='color: green;'> Welcome {$_COOKIE["users"]} </h1>";
+        } else {
+            setcookie("users", "user1", time() + 3600, "/Website");
+            echo "<h1 style='color: white;'>Welcome anonymous</h1>";
+        }
+    }
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -21,26 +36,41 @@ if ($_SESSION[$_COOKIE["users"]] == "admin"){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="refresh" content="300">
     <title>Document</title>
+    <link rel="stylesheet" href="home1.css">
+    <link rel="stylesheet" href="solarSystem.css">
 </head>
 
-<body style="background-color: black;">
-    <h1 style="color: red;">This is Home</h1> <br>
+<body>
+    <h1>This is Home </h1> <br>
 
-    <img src="images\home.jpg" height="100" title="Home" >
-    <audio controls autoplay src="music\past_lives.mp3" > </audio>
-    <audio controls autoplay src="music\my_war.mp3" > </audio>
+    <img src="images\home.jpg" height="100" title="Home">
+    <audio background src="music\past_lives.mp3"> </audio>
+    <audio controls src="music\my_war.mp3"> </audio>
     <form action="home.php" method="post">
-    <input type="submit" name="logout" value="Log out">
+        <input type="submit" name="logout" value="Log out">
+
     </form>
+    <input id ="button2" type="submit" name="clickme" value="Click me">
+    <div id="box1">
+        <div id="box2">
+            <div class="container">
+                <div class="sun"></div>
+                <div class="earth">
+                    <div class="moon"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
 </body>
 
 </html>
 
 <?php
 
-if(isset($_POST["logout"])){
-
-    session_destroy();
-    header("Location: login.php");
+if (isset($_POST["logout"])) {
+end_Section();
 }
+
 ?>
