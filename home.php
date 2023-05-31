b7eaaa9f4e57004f5d1be81b3a4c3ecb97340f87
Nghia <nghiabeo69@gmail.com>
2023-06-01 03:46:08 +0700

    Update home.php

--- home.php
+++ home.php
@@ -1,6 +1,15 @@
 <?php
 // include("solarSystem.html");
 session_start();
+function check_cookie()
+{
+
+    if ($_COOKIE["users"] == "admin") {
+       include("html/login.html");
+    } else {
+        include("html/login1.html");
+    }
+}
 
 function end_Section(){
         session_destroy();
@@ -12,7 +21,7 @@ try {
         end_Section();
     } else {
         if ($_SESSION[$_COOKIE["users"]] == $_SESSION[$_COOKIE["users"]]) {
-            echo "<h1 style='color: green;'> Welcome {$_COOKIE["users"]} </h1>";
+            include("home.html");
         } else {
             setcookie("users", "user1", time() + 3600, "/Website");
             echo "<h1 style='color: white;'>Welcome anonymous</h1>";
@@ -23,52 +32,10 @@ try {
 }
 
 
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="refresh" content="300">
-    <title>Document</title>
-    <link rel="stylesheet" href="home1.css">
-    <link rel="stylesheet" href="solarSystem.css">
-</head>
-
-<body>
-    <h1>This is Home </h1> <br>
-
-    <img src="images\home.jpg" height="100" title="Home">
-    <audio background src="music\past_lives.mp3"> </audio>
-    <audio controls src="music\my_war.mp3"> </audio>
-    <form action="home.php" method="post">
-        <input type="submit" name="logout" value="Log out">
-
-    </form>
-    <input id ="button2" type="submit" name="clickme" value="Click me">
-    <div id="box1">
-        <div id="box2">
-            <div class="container">
-                <div class="sun"></div>
-                <div class="earth">
-                    <div class="moon"></div>
-                </div>
-            </div>
-        </div>
-    </div>
 
 
-</body>
-
-</html>
-
-<?php
 
 if (isset($_POST["logout"])) {
 end_Section();
 }
-
 ?>
