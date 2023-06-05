97da11c384ea00076714a2c46bc4ca87c2937f18
Nghia <nghiabeo69@gmail.com>
2023-06-06 04:03:55 +0700

    Update ViewHWbyTeacher.php

--- Student_Teacher/ViewHWbyTeacher.php
+++ Student_Teacher/ViewHWbyTeacher.php
@@ -24,23 +24,24 @@
 
 <body>
     <h2>List of HomeWork Uploaded</h2>
-    <p>
-        <a href="uploadFile.php">Add New HomeWork</a>
-    </p>
+    <button><a href="../home.php"> Home</a></button>
+    <button><a href="uploadFile.php">Add New HomeWork</a></button>
+
     <table>
         <thead>
             <tr>
                 <th>Challenge ID</th>
                 <th>File Name</th>
+                <th>View</th>
             </tr>
         </thead>
         <tbody>
             <?php
-            $directory = "uploads/"; // Specify the directory path where the files are uploaded
+            $directory = "uploadByTeacher/"; // Specify the directory path where the files are uploaded
             // Get the list of files in the directory
             $files = scandir($directory);
 
-            include("Database/userdb.php");
+            include("../Database/userdb.php");
             // Get the list of files in the directory
             $result = mysqli_query($connect, "SELECT * FROM challenge ORDER BY challengeID ASC");
 
@@ -50,12 +51,14 @@
                     while ($res = mysqli_fetch_assoc($result)) {
                         echo "<td>" . $res['challengeID'] . "</td>";
                         echo "<td>" . $res['challengeName'] . "</td>";
+                        echo "<td><button><a href='viewSubmission.php'>View submission
+                        </a></button></td>";
                         echo "</tr>";
                     }
-                    // echo "<td><a href='viewSubmission.php' target='_blank'>View submission</a></td>";
+                    
                 }
             }
-            echo "<td><a href='viewSubmission.php' target='_blank'>View submission</a></td>";
+            // echo "<td><a href='viewSubmission.php' target='_blank'>View submission</a></td>";
             ?>
         </tbody>
     </table>
